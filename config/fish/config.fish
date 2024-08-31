19b1fcb37641d8699ce25f5fdbaf65bdbf34a23a
Mikołaj Neubauer <nicholas.neubauer.developer@gmail.com>
2024-08-31 19:56:08 +0200

    Removed bat replacement from fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,9 +23,6 @@ set -U __done_notification_urgency_level low
 # Replace ls with eza
 alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
 
-# Replace some more things with better alternatives
-alias cat 'bat --style header --style snip --style changes --style header'
-
 # Get fastest mirrors
 alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
 
